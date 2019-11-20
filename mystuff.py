bed3d56fb457f50883ebb005c9507001a6f4c555
Aly <batcha@hartwick.edu>
2019-11-19 20:41:26 -0500

    finished

---
+++ mystuff.py
@@ -0,0 +1,8 @@
+# this goes in mystuff.py
+def apple():
+    print("I AM APPLES!")
+
+def apple():
+    print("I AM APPLES!")
+# this is just a variable
+tangerine = "Living reflectipons of a dream"
